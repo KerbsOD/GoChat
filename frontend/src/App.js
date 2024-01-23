af5fb57c70122070a66d1ac3f27f03b2e6301a85
KerbsOD <kerbsod@gmail.com>
2024-01-22 21:05:58 -0300

    Agregando multiples accesos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
+// App.js
 import React, { Component } from "react";
+import Header from './components/Header/Header';
+import ChatHistory from "./components/ChatHistory/ChatHistory";
+import "./App.css";
 import { connect, sendMsg } from "./api";
-//import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    connect();
+    this.state = {
+      chatHistory: []
+    }
+  }
+
+  componentDidMount() {
+    connect((msg) => {
+      console.log("New Message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state);
+    });
   }
 
   send() {
@@ -17,6 +31,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <ChatHistory chatHistory={this.state.chatHistory} />
         <button onClick={this.send}>Hit</button>
       </div>
     );
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,28 +1,30 @@
+// api/index.js
 var socket = new WebSocket("ws://localhost:8080/ws");
 
-let connect = () => {
-    console.log("Attemping Connection...");
+let connect = cb => {
+  console.log("Attempting Connection...");
 
-    socket.onopen = () => {
-        console.log("Succesfully Connected");
-    };
+  socket.onopen = () => {
+    console.log("Successfully Connected");
+  };
 
-    socket.onmessage = msg => {
-        console.log(msg);
-    };
-    
-    socket.onclose = event => {
-        console.log("Socket Closed Connection: ", event);
-    };
+  socket.onmessage = msg => {
+    console.log(msg);
+    cb(msg)
+  };
 
-    socket.onerror = error => {
-        console.log("Socket Error: ", error);
-    };
+  socket.onclose = event => {
+    console.log("Socket Closed Connection: ", event);
+  };
+
+  socket.onerror = error => {
+    console.log("Socket Error: ", error);
+  };
 };
 
 let sendMsg = msg => {
-    console.log("sending msg: ", msg);
-    socket.send(msg);
+  console.log("sending msg: ", msg);
+  socket.send(msg);
 };
 
-export {connect, sendMsg}
+export { connect, sendMsg };
