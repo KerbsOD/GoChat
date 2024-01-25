63c60c3b7346e283ba4b209af05e9433a8e3a8f6
KerbsOD <kerbsod@gmail.com>
2024-01-25 19:41:40 -0300

    Nombre del documento

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,38 +7,41 @@ import "./App.css";
 import { connect, sendMsg } from "./api";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chatHistory: []
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            chatHistory: []
+        }
     }
-  }
 
-  componentDidMount() {
-    connect((msg) => {
-      console.log("New Message")
-      this.setState(prevState => ({
-        chatHistory: [...this.state.chatHistory, msg]
-      }))
-      console.log(this.state);
-    });
-  }
-  send(event) {
-    if(event.keyCode === 13) {
-      sendMsg(event.target.value);
-      event.target.value = "";
+    componentDidMount() {
+        document.title = "GoChat";  
+        connect((msg) => {
+            console.log("New Message")
+            this.setState(prevState => ({
+                chatHistory: [...this.state.chatHistory, msg]
+            }))
+            console.log(this.state);
+        });
     }
-  }
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <ChatHistory chatHistory={this.state.chatHistory} />
-        <ChatInput send={this.send} />
 
-      </div>
-    );
-  }
+    send(event) {
+        if(event.keyCode === 13) {
+            sendMsg(event.target.value);
+            event.target.value = "";
+        }
+    }
+
+    render() {
+        return (
+        <div className="App">
+            <Header />
+            <ChatHistory chatHistory={this.state.chatHistory} />
+            <ChatInput send={this.send} />
+        </div>
+        );
+    }
 }
 
 export default App;
