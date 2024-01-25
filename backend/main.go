107e7b5e3e391a37e767c40ba543e222f7ed91cd
KerbsOD <kerbsod@gmail.com>
2024-01-25 00:06:13 -0300

    Detalles del backend

--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,15 @@ import (
 	"github.com/KerbsOD/LordsMessage/pkg/websocket"
 )
 
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
+
 func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
 	fmt.Println("WebSocket Endpoint Hit")
 	conn, err := websocket.Upgrade(w, r)
@@ -21,15 +30,6 @@ func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
 
 	pool.Register <- client
 	client.Read()
-}
-
-func setupRoutes() {
-	pool := websocket.NewPool()
-	go pool.Start()
-
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(pool, w, r)
-	})
 }
 
 func main() {
