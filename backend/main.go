73318d7e25512218ba67384f9d1e8435c748d3eb
KerbsOD <kerbsod@gmail.com>
2024-01-31 10:51:17 -0300

    Top down approach

--- backend/main.go
+++ backend/main.go
@@ -7,43 +7,37 @@ import (
 	"github.com/KerbsOD/GoChat/pkg/websocket"
 )
 
-// We initialize our channels via the NewPool() function
-// The pool handling must be concurrent with the TCP listener in main.
+func main() {
+	fmt.Println("===== GoChat v1.1 =====")
+	pool := initializePool()
+	setupRoutes(pool)
+	http.ListenAndServe(":8080", nil)
+}
+
 func initializePool() *websocket.Pool {
 	pool := websocket.NewPool()
 	go pool.Start()
 	return pool
 }
 
-// Registration of the patterns with their corresponding function handler.
 func setupRoutes(pool *websocket.Pool) {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(pool, w, r)
+		serveWebSocket(pool, w, r)
 	})
 }
 
-// We upgrade the CLient's HTTP connection to Websocket protocol and register it onto the pool.
-// The client now will have access to broadcasted messages until it's disconnected.
-func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
-	fmt.Println("WebSocket Endpoint Hit")
+func serveWebSocket(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
 	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
 		fmt.Fprintf(w, "%+V\n", err)
 	}
 
 	client := &websocket.Client{
-		ID:   "Octo",
+		ID:   "Norber",
 		Conn: conn,
 		Pool: pool,
 	}
 
 	pool.Register <- client
 	client.Read()
-}
-
-func main() {
-	fmt.Println("===== GoChat v1.0 =====")
-	pool := initializePool()
-	setupRoutes(pool)
-	http.ListenAndServe(":8080", nil)
 }
