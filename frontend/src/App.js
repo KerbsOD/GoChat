7a87726959053661c78be3458c104276b2625e11
KerbsOD <kerbsod@gmail.com>
2024-02-06 15:38:22 -0300

    App.js from calss component to functional component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,39 @@
-// App.js
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import ChatHistory from "./components/ChatHistory/ChatHistory";
 import ChatInput from "./components/ChatInput";
 import "./App.css";
 import { connect, sendMsg } from "./api";
 
-class App extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            chatHistory: []
-        }
-    }
+export default function App() {
+    const [chatHistory, setChatHistory] = useState([]);
 
-    componentDidMount() {
-        document.title = "GoChat";  
-        connect((msg) => {
+    useEffect(() => {
+        document.title = "GoChat";
+
+        const handleMessage = (msg) => {
             console.log("New Message")
-            this.setState(prevState => ({
-                chatHistory: [...this.state.chatHistory, msg]
-            }))
-            console.log(this.state);
-        });
-    }
+            setChatHistory(prevChatHistory => [...prevChatHistory, msg]);
+            console.log(chatHistory);
+        };
 
-    send(event) {
-        if(event.keyCode === 13) { // enter
+        connect(handleMessage);
+
+    }, []); 
+
+    const send = (event) => {
+        if (event.keyCode === 13) { // enter
             sendMsg(event.target.value);
             event.target.value = "";
         }
-    }
+    };
 
-    render() {
-        return (
+    return (
         <div className="App">
             <Header />
-            <ChatHistory chatHistory={this.state.chatHistory} />
-            <ChatInput send={this.send} />
+            <ChatHistory chatHistory={chatHistory} />
+            <ChatInput send={send} />
         </div>
-        );
-    }
-}
+    );
+};
 
-export default App;
