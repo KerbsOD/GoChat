afaf0cff4ed5999e10694bec5fc952895b619b44
Octo Kerbs <kerbsod@gmail.com>
2024-03-11 06:49:37 -0300

    comments

--- frontend/src/components/ChatInput/ChatInput.jsx
+++ frontend/src/components/ChatInput/ChatInput.jsx
@@ -3,6 +3,7 @@ import "./ChatInput.scss";
 export default function ChatInput({ send }) {
     return (
         <div className="ChatInput">
+            {/*Si apreto alguna tecla, usa la funcion send para handlearlo*/}
             <input onKeyDown={send}/>
         </div>
     )
