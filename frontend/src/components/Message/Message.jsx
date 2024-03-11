ffc71c70f7be6fdc74dc9568518274daaa81ad7c
Octo Kerbs <kerbsod@gmail.com>
2024-03-11 07:16:45 -0300

    Added status to client. Covers cases were a client enters or lefts the chat

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -2,11 +2,21 @@ import "./Message.scss";
 
 export default function Message ({message}) {
     let temp = JSON.parse(message);
-    const { sender, body } = temp
-    
+    const {statusmessage, sender, body,} = temp
+
+    let postMessage = (<div><strong>{sender}</strong>: {body}</div>)
+
+    if (statusmessage === 0) {
+        postMessage = (<div><strong>{sender} has joined the chat </strong></div>)
+    }
+
+    if (statusmessage === 2) {
+        postMessage = (<div><strong>{sender} has left the chat </strong></div>)
+    }
+
     return (
         <div className="Message">
-            <div><strong>{sender}</strong>: {body}</div>
+            {postMessage}
         </div>
     )
 }
